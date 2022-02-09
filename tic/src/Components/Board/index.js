2c99ffcddf11fd0549d5112a1973e7f7bb194380
Dan-Sutton <dewasutton@gmail.com>
2022-02-09 12:36:55 +0000

    handle board function added

--- tic/src/Components/Board/index.js
+++ tic/src/Components/Board/index.js
@@ -1,28 +1,40 @@
-import React from 'react'
-import "./style.css"
-import Square from '../Square'
+import React from "react";
+import "./style.css";
+import Square from "../Square";
 
-function Board() {
+function Board({ handleBoard }) {
   return (
-    <div className='board'>
-        <h1>Board</h1>
-   <div className='row-1'>
-    <Square />
-    <Square />
-    <Square />
+    <div className="board">
+      <h1>Board</h1>
+      <div className="row-1">
+        <Square
+          onClick={() => {
+            handleBoard(0);
+          }}
+        />
+        <Square
+          onClick={() => {
+            handleBoard(1);
+          }}
+        />
+        <Square
+          onClick={() => {
+            handleBoard(2);
+          }}
+        />
+      </div>
+      <div className="row-2">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="row-3">
+        <Square />
+        <Square />
+        <Square />
+      </div>
     </div>
-    <div className='row-2'>
-    <Square />
-    <Square />
-    <Square /> 
-    </div>
-   <div className='row-3'> 
-    <Square />
-    <Square />
-    <Square />
-    </div>
-    </div>
-  )
+  );
 }
 
-export default Board
+export default Board;
--- tic/src/Components/Game/index.js
+++ tic/src/Components/Game/index.js
@@ -1,18 +1,35 @@
-import { useState} from 'react'
-import "./style.css"
-import Board from '../Board'
+import { useState } from "react";
+import "./style.css";
+import Board from "../Board";
 
 function Game() {
-    const [boardValue, setBoardValue] = useState([null,null,null,null,null,null,null,null,null])
-    const [playerTurn, setPlayerTurn] = useState("Player 1")
-  return (
-    <div className='game'>
-        <Board />
-    <h3>Player turn: X</h3>
-    <h3>Winner: X</h3>
-    </div>
+  const [boardValue, setBoardValue] = useState([
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+  ]);
+  const [playerTurn, setPlayerTurn] = useState("Player 1");
 
-  )
+  function handleBoard(index) {
+    const spreadBoard = [...boardValue];
+    const updateBoard = spreadBoard.splice(index, 1, "Place Holder");
+    setBoardValue(updateBoard);
+    console.log(boardValue);
+  }
+
+  return (
+    <div className="game">
+      <Board handleBoard={handleBoard} />
+      <h3>Player turn: {playerTurn}</h3>
+      <h3>Winner: X</h3>
+    </div>
+  );
 }
 
-export default Game
+export default Game;
