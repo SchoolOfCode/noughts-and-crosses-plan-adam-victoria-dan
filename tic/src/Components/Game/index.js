9afa32a3795bd69df1ce390afa832c2d0d38deaf
Victoriam97 <victoriameah@hotmail.com>
2022-02-09 12:18:03 +0000

    Game states made

--- tic/src/Components/Game/index.js
+++ tic/src/Components/Game/index.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import { useState} from 'react'
 import "./style.css"
 import Board from '../Board'
 
 function Game() {
+    const [boardValue, setBoardValue] = useState([null,null,null,null,null,null,null,null,null])
+    const [playerTurn, setPlayerTurn] = useState("Player 1")
   return (
     <div className='game'>
         <Board />
